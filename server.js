1efe3e17c041bc1f4aaf8bdc231755c356f00a13
Vikas Parmar <95103544+vikasxci@users.noreply.github.com>
2026-04-13 14:28:35 +0530

    Add saloon route to server API

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.use('/api/shop', require('./routes/shop'));
 app.use('/api/uptime', require('./routes/uptime'));
 app.use('/api/inventory', require('./routes/inventory'));
 app.use('/api/restaurant', require('./routes/restaurant'));
+app.use('/api/saloon', require('./routes/saloon'));
 
 // Health check endpoint for deployment monitoring
 app.get('/api/health', (req, res) => {
